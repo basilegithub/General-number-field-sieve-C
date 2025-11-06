d51d5aa97a5e3d15bdad9428ec1fd0abb274122b
basilegithub <basile.jezequel@gmail.com>
2025-11-06 22:14:39 +0100

    created the main file, and added the file that handles logs

---
+++ src/main.c
@@ -0,0 +1,18 @@
+// Main folder of the project
+
+#include <stdio.h>
+
+int main()
+{
+    printf("program started\n");
+
+    // The GNFS algorithm is split in 4 main steps
+
+    // Polynomial selection
+
+    // Sieving
+
+    // Linear algebra
+
+    // Square root extraction
+}
