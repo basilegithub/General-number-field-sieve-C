13399ab5363bd95c2b629fe67f7b4d93d314557b
basilegithub <basile.jezequel@gmail.com>
2025-11-06 22:17:57 +0100

    Added interactive input at runtime

--- src/main.c
+++ src/main.c
@@ -1,10 +1,26 @@
-// Main folder of the project
+// Main file of the project
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+#include <gmp.h>
 
 int main()
 {
     printf("program started\n");
+
+    char number[100];
+    printf("enter the number you wish to factor : ");
+    fgets(number, 100, stdin);
+
+    mpz_t n;
+
+    mpz_init(n);
+
+    mpz_init_set_str(n, number, 10);
 
     // The GNFS algorithm is split in 4 main steps
 
@@ -15,4 +31,8 @@ int main()
     // Linear algebra
 
     // Square root extraction
+
+
+
+    mpz_clear(n);
 }
