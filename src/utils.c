2e1e4befde8a4a64c21808eb0bac0334a2a6aa2e
basilegithub <basile.jezequel@gmail.com>
2025-11-14 15:28:07 +0100

    Added the functions to compute the exponential function

--- src/utils.c
+++ src/utils.c
@@ -45,6 +45,79 @@ void natural_log(mpf_t res, mpf_t x, mpf_t ln2, mpf_t e)
     mpf_clears(tmpf, a, NULL);
 }
 
+void recursive_exp(mpf_t res, mpz_t pow, mpf_t e)
+{
+    if (!mpz_cmp_ui(pow, 0)) mpf_set_ui(res, 1);
+    else if (!mpz_cmp_ui(pow, 1)) mpf_set(res, e);
+    else
+    {
+        mpf_t tmpf;
+        mpf_init(tmpf);
+        mpz_t tmp;
+        mpz_init(tmp);
+
+        if (mpz_even_p(pow))
+        {
+            mpz_div_ui(tmp, pow, 2);
+            recursive_exp(tmpf, tmp, e);
+            mpf_mul(res, tmpf, tmpf);
+        }
+        else
+        {
+            mpz_div_ui(tmp, pow, 2);
+            recursive_exp(tmpf, tmp, e);
+            mpf_mul(tmpf, tmpf, tmpf);
+            mpf_mul(res, e, tmpf);
+        }
+        mpf_clear(tmpf);
+        mpz_clear(tmp);
+    }
+}
+
+void myexp(mpf_t res, mpf_t x, mpf_t e)
+{
+    int flag = 0;
+    if (mpf_cmp_ui(x, 0) < 0)
+    {
+        flag = 1;
+        mpf_neg(x, x);
+    }
+    mpf_t tmp_res, rest;
+    mpz_t tmp;
+
+    mpf_inits(tmp_res, rest, NULL);
+    mpz_init(tmp);
+
+    mpz_set_f(tmp, x);
+    mpf_set_z(rest, tmp);
+    mpf_sub(rest, x, rest);
+
+    recursive_exp(tmp_res, tmp, e);
+
+    mpf_t tmp_rest, fact, tmpf, tmpf2;
+
+    mpf_init_set(tmp_rest, rest);
+    mpf_init_set_ui(fact, 1);
+    mpf_init_set_ui(tmpf, 1);
+    mpf_init(tmpf2);
+
+    for (unsigned long i = 2 ; i <= 10 ; i++)
+    {
+        mpf_div(tmpf2, tmp_rest, fact);
+        mpf_add(tmpf, tmpf, tmpf2);
+        mpf_mul_ui(fact, fact, i);
+        mpf_mul(tmp_rest, tmp_rest, rest);
+    }
+    mpf_mul(res, tmp_res, tmpf);
+    if (flag)
+    {
+        mpf_set_ui(tmpf, 1);
+        mpf_div(res, tmpf, res);
+    }
+    mpz_clear(tmp);
+    mpf_clears(tmpf, tmpf2, rest, tmp_res, tmp_rest, fact, NULL);
+}
+
 void nth_root(mpf_t r, mpf_t x, unsigned long n)
 {
     mpf_t tmpf, tmpf2, tmpf3, tmpf4;
@@ -58,7 +131,7 @@ void nth_root(mpf_t r, mpf_t x, unsigned long n)
 
     mpf_pow_ui(tmpf4, tmpf3, n);
 
-    for (size_t i = 0 ; i < 50 ; i++)
+    for (size_t i = 0 ; i < 100 ; i++)
     {
         if (mpf_cmp(tmpf4, x) < 0) mpf_set(tmpf, tmpf3);
         else mpf_set(tmpf2, tmpf3);
@@ -102,10 +175,16 @@ void compute_smooth_bound(mpz_t n, mpz_t smooth_bound, mpf_t ln2, mpf_t e)
     natural_log(tmpf, tmpf, ln2, e);
     mpf_mul_ui(tmpf, tmpf, 8);
     mpf_div_ui(tmpf, tmpf, 9);
-    
+    nth_root(tmpf, tmpf, 3);
 
     natural_log(tmpf2, tmpf2, ln2, e);
     natural_log(tmpf2, tmpf2, ln2, e);
+    mpf_pow_ui(tmpf2, tmpf2, 2);
+    nth_root(tmpf2, tmpf2, 3);
+
+    mpf_mul(tmpf3, tmpf, tmpf2);
+
+    ex
 
     mpf_clears(tmpf, tmpf2, tmpf3, tmpf4, NULL);
 }
