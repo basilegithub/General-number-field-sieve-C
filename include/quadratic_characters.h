9dc9b6c4525b373338fbf8b52e3c7a6e9bf2acb3
basilegithub <basile.jezequel@gmail.com>
2025-11-17 17:13:47 +0100

    Built the function for generating the quadratic characters. Early stop if a divisor is found

--- include/quadratic_characters.h
+++ include/quadratic_characters.h
@@ -16,12 +16,13 @@ typedef struct quadratic_character
 typedef struct
 {
     quadratic_character *start;
+    quadratic_character *end;
 } quadratic_character_base; // Dynamic algebraic base
 
 // Functions definition
 
 void quadratic_character_base_init(quadratic_character_base *b);
-void algebraic_base_clear(algebraic_base *b);
-void build_algebraic_base(algebraic_base *b, dyn_array_classic primes, polynomial_mpz g_x, mpz_t n);
+void algebraic_base_clear(quadratic_character_base *b);
+void create_quadratic_characters_base(quadratic_character_base *q_base, polynomial_mpz f, polynomial_mpz f_derivative, mpz_t n, mpz_t leading_coeff, unsigned long required_size, unsigned long start_prime);
 
 #endif // QUADRATIC_CHARACTERS_H
