ffd649b45ee85d61c4d12eee8084e8afdcef9099
basilegithub <basile.jezequel@gmail.com>
2025-12-10 16:27:33 +0100

    Coded the function that computes free relations from the algebraic factor base

--- include/init_functions.h
+++ include/init_functions.h
@@ -10,4 +10,14 @@ void initialize_params(gmp_randstate_t state, mpf_t ln10, mpf_t ln2, mpf_t e);
 void compute_smooth_bound(mpz_t n, mpz_t smooth_bound, mpf_t ln2, mpf_t e);
 void compute_logs(dyn_array_classic *logs, dyn_array_classic primes);
 
+void compute_free_relations(
+    nfs_relations * restrict relations,
+    const algebraic_base * restrict alg_base,
+    const unsigned long * restrict divide_leading,
+    mpz_t leading_coeff,
+    mpz_t m1,
+    const size_t len_divide_leading,
+    const unsigned long degree
+);
+
 #endif // INIT_FUNCTIONS_H
