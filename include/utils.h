d0c57b0da9635e9e0a95b702278889fd5369983d
basilegithub <basile.jezequel@gmail.com>
2025-12-11 17:33:53 +0100

    Type descriptors for utils.c and utils.h

--- include/utils.h
+++ include/utils.h
@@ -7,16 +7,23 @@
 #include "dynamic_arrays.h"
 
 void compute_e(mpf_t e);
-void recursive_exp(mpf_t res, mpz_t pow, mpf_t e);
-void myexp(mpf_t res, mpf_t x, mpf_t e);
-void natural_log(mpf_t res, mpf_t x, mpf_t ln2, mpf_t e);
-void nth_root(mpf_t r, mpf_t x, unsigned long n);
-void initialize_params(gmp_randstate_t state, mpf_t ln10, mpf_t ln2, mpf_t e);
-void compute_smooth_bound(mpz_t n, mpz_t smooth_bound, mpf_t ln2, mpf_t e);
+
+void recursive_exp(mpf_t res, mpz_t pow, const mpf_t e);
+
+void myexp(mpf_t res, mpf_t x, const mpf_t e);
+
+void natural_log(mpf_t res, mpf_t x, const mpf_t ln2, const mpf_t e);
+
+void nth_root(mpf_t r, const mpf_t x, const unsigned long n);
+
 unsigned long gcd(unsigned long a, unsigned long b);
-bool fermat_primality(mpz_t n);
-int my_legendre(mpz_t n, unsigned long p);
+
+bool fermat_primality(const mpz_t n);
+
+int my_legendre(const mpz_t n, unsigned long p);
+
 void sqrt_mod(mpz_t n, const unsigned long p, gmp_randstate_t state);
-void convert_to_vec(mpz_t embedding, unsigned long relations_len, bool tmp_vec[relations_len]);
+
+void convert_to_vec(mpz_t embedding, unsigned long relations_len, bool * restrict tmp_vec);
 
 #endif // UTILS_H
