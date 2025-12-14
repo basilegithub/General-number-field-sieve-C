036e7329c27e0ecd51d01b1cec6a02f69ed9a14a
Basile Jezequel <basile.jezequel@gmail.com>
2025-12-14 22:53:20 +0100

    Finished writing the function to compute the table of coefficients for the dicjman function

--- src/dickman_table.c
+++ src/dickman_table.c
@@ -18,7 +18,7 @@ void compute_dickman_coeffs(
 
     table->coefficients = calloc(table->nb_coefficients * table->nb_lines, sizeof(double));
 
-    table->coefficients[0] = 0?3068528 // 1 - ln(2)
+    table->coefficients[0] = 0.3068528 // 1 - ln(2)
     
     for (size_t i = 1 ; i < table->nb_coefficients ; i++)
     {
@@ -32,8 +32,18 @@ void compute_dickman_coeffs(
             double c = 0;
             for (size_t k = 0 ; k < j ; k++)
             {
-                c += table->coefficients[(i-2)*table->nb_coefficients + k - 1]/(double)(j * my_power())
+                c += table->coefficients[(i-3)*table->nb_coefficients + k]/(double)(j * my_power(i, j - k));
             }
+            table->coefficients[(i-2)*table->nb_coefficients + j] = c;
         }
+
+        double c = 0;
+
+        for (size_t j = 1 ; j < table->nb_coefficients ; j++)
+        {
+            c += table->coefficients[(i-2)*table->nb_coefficients + j]/(double)(j+1);
+        }
+
+        table->coefficients[(i-2)*table->nb_coefficients] = c/(i-1);
     }
 }
