1f24c54410916e661f6d1efa5d08020c13e1dfa2
basilegithub <basile.jezequel@gmail.com>
2025-12-25 21:58:54 +0100

    Wrote the function to compute Epscore

--- include/utils_polynomial_selection.h
+++ include/utils_polynomial_selection.h
@@ -7,15 +7,6 @@
 #include "polynomial_structures.h"
 #include "polynomial_functions.h"
 
-void get_Lnorm(
-    mpz_t res,
-    const polynomial_mpz * restrict F,
-    const unsigned long skew,
-    const unsigned long smoothness_bound,
-    const mpf_t ln2,
-    const mpf_t e
-);
-
 void evaluate_legendre(
     const unsigned long n,
     const double x,
@@ -28,5 +19,28 @@ void nodes_and_weights_gauss(
     double * restrict x,
     double * restrict w
 );
+
+void get_Lnorm(
+    mpz_t res,
+    const polynomial_mpz * restrict F,
+    const unsigned long skew,
+    const unsigned long smoothness_bound,
+    const mpf_t ln2,
+    const mpf_t e
+);
+
+double get_Escore(
+    const polynomial_mpz * restrict f,
+    const polynomial_mpz * restrict g,
+    const double alpha,
+    const unsigned long smoothness_bound,
+    const unsigned long x_limit,
+    const unsigned long y_limit,
+    dickman_table * restrict table,
+    const mpf_t ln2,
+    const mpf_t e
+);
+
+
 
 #endif // UTILS_POLYNOMIAL_SELECTION
--- src/utils_polynomial_selection.c
+++ src/utils_polynomial_selection.c
@@ -64,6 +64,75 @@ void nodes_and_weights_gauss(
     }
 }
 
+void get_Lnorm(
+    mpz_t res,
+    const polynomial_mpz * restrict F,
+    const unsigned long skew,
+    const unsigned long smoothness_bound,
+    const mpf_t ln2,
+    const mpf_t e
+)
+{
+    unsigned long square_root = isqrt(skew);
+    unsigned long n = F->degree + 1;
+
+    mpz_t base_X, base_Y;
+    mpz_init_set_ui(base_X, smoothness_bound);
+    mpz_init_set_ui(base_Y, smoothness_bound);
+
+    mpz_mul_ui(base_X, base_X, square_root);
+    mpz_div_2exp(base_X, base_X, 1);
+
+    mpz_div_ui(base_Y, base_Y, square_root);
+    mpz_add_ui(base_Y, base_Y, 1);
+
+    mpz_t current_X, current_Y;
+    mpz_init_set(current_X, base_X);
+    mpz_init_set(current_Y, base_Y);
+
+    mpz_pow_ui(current_X, current_X, n);
+
+    mpz_mul(base_X, base_X, base_X);
+    mpz_mul(base_Y, base_Y, base_Y);
+
+    mpz_set_ui(res, 0);
+
+    mpz_t tmp, tmp2;
+    mpz_inits(tmp, tmp2, NULL);
+
+    for (size_t i = 0 ; i < n ; i += 2)
+    {
+        mpz_mul_2exp(tmp, current_X, 1);
+        mpz_sub_ui(tmp2, current_X, 1);
+
+        mpz_mul(tmp, tmp, tmp2);
+        mpz_mul(tmp, tmp, F->coeffs[i]);
+
+        mpz_set_ui(tmp2, (i+1)*(n-i));
+        mpz_div(tmp, tmp, tmp2);
+
+        mpz_add(res, res, tmp);
+
+        mpz_divexact(current_X, current_X, base_X);
+        mpz_mul(current_Y, current_Y, base_Y);
+    }
+
+    mpz_abs(res, res);
+    
+    mpf_t res_f, x;
+    mpf_init(res_f);
+    mpf_init(x);
+
+    mpf_set_prec(x, 2048);
+    mpf_set_z(x, res);
+
+    natural_log(res_f, x, ln2, e);
+    mpz_set_f(res, res_f);
+
+    mpz_clears(base_X, base_Y, current_X, current_Y, tmp, tmp, NULL);
+    mpf_clears(res_f, x, NULL);
+}
+
 double get_Escore(
     const polynomial_mpz * restrict f,
     const polynomial_mpz * restrict g,
@@ -152,71 +221,118 @@ double get_Escore(
 
 }
 
-void get_Lnorm(
-    mpz_t res,
-    const polynomial_mpz * restrict F,
-    const unsigned long skew,
+void get_Epscore(
+    const polynomial_mpz * restrict f,
+    const polynomial_mpz * restrict g,
+    const double *alpha,
     const unsigned long smoothness_bound,
+    const unsigned long x_limit,
+    const unsigned long y_limit,
+    dickman_table * restrict table,
     const mpf_t ln2,
     const mpf_t e
 )
 {
-    unsigned long square_root = isqrt(skew);
-    unsigned long n = F->degree + 1;
+    double k = *alpha;
+    double l = alpha[1];
+    double c = alpha[2];
 
-    mpz_t base_X, base_Y;
-    mpz_init_set_ui(base_X, smoothness_bound);
-    mpz_init_set_ui(base_Y, smoothness_bound);
+    unsigned long n = 32;
 
-    mpz_mul_ui(base_X, base_X, square_root);
-    mpz_div_2exp(base_X, base_X, 1);
+    double log_B;
 
-    mpz_div_ui(base_Y, base_Y, square_root);
-    mpz_add_ui(base_Y, base_Y, 1);
+    mpf_t tmpf, tmpf2;
+    mpf_init_set_ui(tmpf, smoothness_bound);
+    mpf_init(tmpf2);
 
-    mpz_t current_X, current_Y;
-    mpz_init_set(current_X, base_X);
-    mpz_init_set(current_Y, base_Y);
+    natural_log(tmpf2, tmpf, ln2, e);
 
-    mpz_pow_ui(current_X, current_X, n);
+    log_B = mpf_get_d(tmpf2);
 
-    mpz_mul(base_X, base_X, base_X);
-    mpz_mul(base_Y, base_Y, base_Y);
+    double *weights_mu = calloc(n, sizeof(double));
+    double *nodes_mu = calloc(n, sizeof(double));
+    nodes_and_weights_gauss(n, nodes_mu, weights_mu);
 
-    mpz_set_ui(res, 0);
+    double a, b;
+    a = c;
+    b = 6*c; // pi
 
-    mpz_t tmp, tmp2;
-    mpz_inits(tmp, tmp2, NULL);
-
-    for (size_t i = 0 ; i < n ; i += 2)
+    for (size_t i = 0 ; i < n ; i++)
     {
-        mpz_mul_2exp(tmp, current_X, 1);
-        mpz_sub_ui(tmp2, current_X, 1);
-
-        mpz_mul(tmp, tmp, tmp2);
-        mpz_mul(tmp, tmp, F->coeffs[i]);
-
-        mpz_set_ui(tmp2, (i+1)*(n-i));
-        mpz_div(tmp, tmp, tmp2);
-
-        mpz_add(res, res, tmp);
-
-        mpz_divexact(current_X, current_X, base_X);
-        mpz_mul(current_Y, current_Y, base_Y);
+        // Shift nodes and weights from [-1, 1] to the required interval [c, 6c]
+        nodes_mu[i] = 0.5 * (b - a) * nodes_mu[i] + 0.5 * (b + a);
+        weights_mu[i] = 0.5 * (b - a) * weights_mu[i];
     }
 
-    mpz_abs(res, res);
-    
-    mpf_t res_f, x;
-    mpf_init(res_f);
-    mpf_init(x);
+    double *weights_th = calloc(n, sizeof(double));
+    double *nodes_th = calloc(n, sizeof(double));
+    nodes_and_weights_gauss(n, nodes_th, weights_th);
 
-    mpf_set_prec(x, 2048);
-    mpf_set_z(x, res);
+    double a, b;
+    a = c;
+    b = 6*c; // pi
 
-    natural_log(res_f, x, ln2, e);
-    mpz_set_f(res, res_f);
+    for (size_t i = 0 ; i < n ; i++)
+    {
+        // Shift nodes and weights from [-1, 1] to the required interval [0, pi]
+        nodes_th[i] = 0.5 * (b - a) * nodes_th[i] + 0.5 * (b + a);
+        weights_th[i] = 0.5 * (b - a) * weights_th[i];
+    }
 
-    mpz_clears(base_X, base_Y, current_X, current_Y, tmp, tmp, NULL);
-    mpf_clears(res_f, x, NULL);
+    double res = 0.0;
+
+    mpz_t X_mpz, Y_mpz, res1, res2;
+    mpz_inits(X_mpz, Y_mpz, res1, res2, NULL);
+
+    for (size_t i = 0 ; i < n ; i++)
+    {
+        double non_central_eval = non_central(k, l, nodes_mu[i]);
+
+        for (size_t j = 0 ; j < n ; j++)
+        {
+            double angle = nodes_th[j];
+
+            double X = (double)x_limit * cos(angle);
+            double Y = (double)(y_limit - 1) * sin(angle) + 1;
+
+            mpz_set_d(X_mpz, X);
+            mpz_set_d(Y_mpz, Y);
+
+            evaluate_homogeneous(res1, f, Y_mpz, Y_mpz);
+            mpz_abs(res1, res1);
+            mpf_set_z(tmpf, res1);
+            natural_log(tmpf2, tmpf, ln2, e);
+            mpf_set_d(tmpf, nodes_mu[i] - c);
+            mpf_sub(tmpf2, tmpf2, tmpf);
+            mpf_set_d(tmpf, log_B);
+            mpf_div(tmpf2, tmpf2, tmpf);
+
+            mpz_set_d(res1, evaluate_dickman(table, mpf_get_d(tmpf2)));
+
+            evaluate_homogeneous(res2, g, Y_mpz, Y_mpz);
+            mpz_abs(res1, res1);
+            mpf_set_z(tmpf, res1);
+            natural_log(tmpf2, tmpf, ln2, e);
+            mpf_set_d(tmpf, log_B);
+            mpf_div(tmpf2, tmpf2, tmpf);
+
+            mpz_set_d(res2, evaluate_dickman(table, mpf_get_d(tmpf2)));
+
+            mpz_mul(res1, res1, res2);
+            mpf_set_z(tmpf, res1);
+            mpf_set_d(tmpf2, weights_mu[i]);
+            mpf_mul(tmpf, tmpf, tmpf2);
+            mpf_set_d(tmpf2, weights_th[j]);
+            mpf_mul(tmpf, tmpf, tmpf2);
+            mpf_set_d(tmpf2, non_central_eval);
+            mpf_mul(tmpf, tmpf, tmpf2);
+            
+            res += mpf_get_d(tmpf);
+        }
+    }
+
+    mpz_clears(X_mpz, Y_mpz, res1, res2, NULL);
+
+    mpf_clears(tmpf, tmpf2, NULL);
+
 }
